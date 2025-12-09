8e7fc839a7277247a74c2923a57892ce5fd17d0b
royhansen99 <roy@royhansen.no>
2025-12-09 10:17:09 +0100

    Bump version to 0.1.2

--- Arena.h
+++ Arena.h
@@ -1,6 +1,6 @@
 /*
  * Package: arena_pool_cpp
- * Version: 0.1.1
+ * Version: 0.1.2
  * License: MIT
  * Github: https://github.com/royhansen99/arena-pool-cpp 
  * Author: Roy Hansen (https://github.com/royhansen99)
